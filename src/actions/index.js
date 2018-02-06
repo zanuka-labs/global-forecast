008b1b1a95d23a97d669bbd47fba8aebdecd14e6
Method <git@zanuka.com>
2018-02-06 17:20:20 +1300

    fetchWeather action creator added :: axios request setup :: search by city,country

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,17 @@
+import axios from 'axios'
+
 const API_KEY = '8a1eeef220eedadb18eae7cc2e4c044c'
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city, country) {
+  const url = `${ROOT_URL}&id=${city},${country}`
+  const request = axios.get(url)
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
 
